22c6acde134c60db4744b5b5731a38d1bf103135
Edson <edson.rodgon@gmailcom>
2022-12-05 07:40:58 -0300

    Initial commit. Product and Category classes created

---
+++ Domain/Products/Products.cs
@@ -0,0 +1,14 @@
+﻿using IWantAPP.Domain.Products;
+
+namespace IWantApp.Domain.Products;
+
+public class Products
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+}
